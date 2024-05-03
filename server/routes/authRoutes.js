e72a9f705dd5939f717a9844d34adbf940f80b4d
UjjwalSingh <30306011+Ujjwal1629@users.noreply.github.com>
2024-05-03 15:37:30 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,7 +10,7 @@ const {
 router.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://w3automation-frontend.vercel.app",
   })
 );
 router.get("/", test);
