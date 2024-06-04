35cf7a650c1b91a0fd183fe3e6059b01210523d6
hemantgandhi12g <hemantgandhi259@gmail.com>
2024-06-04 16:59:32 +0530

    Added some text in First learning block

--- client/src/Pages/Selenium.jsx
+++ client/src/Pages/Selenium.jsx
@@ -11,30 +11,75 @@ export default function Selenium() {
       <div className="main">
         <div className="container-left">
           <div className="container-links">
-            <h1>JS Tutorials</h1>
+            <h1>Selenium With Java</h1>
             <a className="links" href="">
-              JS Home
+              Pre-Requisites For Selenium With Java
+            </a>
+            <a className="links" href="#Intro-Java">
+              Introduction To Java
+            </a>
+            <a className="links" href="#Class-Object">
+              Classes And Objects
             </a>
             <a className="links" href="">
-              JS Introduction
+              Basic Java Programming and Features
+            </a>
+            <a className="links" href="#java-method">
+              Methods Introduction
             </a>
             <a className="links" href="">
-              JS Statements
+              Types Of Methods
             </a>
             <a className="links" href="">
-              JS Data Types
+              Methods - Static and Non Static
             </a>
             <a className="links" href="">
-              JS Operators
+              If and Else Statement
             </a>
             <a className="links" href="">
-              JS Loops
+              For Loop
             </a>
             <a className="links" href="">
-              JS Functions
+              Nested For Loop
             </a>
             <a className="links" href="">
-              JS Objects
+              For Loop
+            </a>
+            <a className="links" href="">
+              Method - Static and Non Static
+            </a>
+            <a className="links" href="">
+              If and Else Statement
+            </a>
+            <a className="links" href="">
+              Break and Continue Statement
+            </a>
+            <a className="links" href="">
+              While and Do While Loop
+            </a>
+            <a className="links" href="">
+              Arrays 2D and 3D
+            </a>
+            <a className="links" href="">
+              OOPs Concepts
+            </a>
+            <a className="links" href="">
+              Exception Handling
+            </a>
+            <a className="links" href="">
+              Java Collections
+            </a>
+            <a className="links" href="">
+              Introduction To Selenium WebDriver
+            </a>
+            <a className="links" href="">
+              Finding WebElement By Locators
+            </a>
+            <a className="links" href="">
+              Handling Dropdowns
+            </a>
+            <a className="links" href="">
+              Handling 
             </a>
           </div>
         </div>
@@ -44,9 +89,61 @@ export default function Selenium() {
             <button className="next">Next</button>
           </div>
           <div className="main-page">
-            <div id="JS-Home">
-              <h1>JavaScript Tutorial</h1>
+            <div id="Introduction To Java Intro-Java" class = "IntroJava">
+              <h1>Introduction To Java</h1>
               <p>
+                What is Java?
+                Java is a very popular pro
+                JavaScript is the world's most popular programming language.
+                JavaScript is the programming language of the Web. JavaScript is
+                easy to learn. This tutorial will teach you JavaScript from
+                basic to advanced.
+              </p>
+            </div>
+            <div id="Classes And Objects Class-Object" class = "IntroJava">
+              <h1>Classes And Objects</h1>
+              <p>
+               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus iure sequi vitae dicta quis ad, quibusdam corrupti sapiente omnis ducimus impedit facilis voluptas? Illo rerum, est possimus maxime cupiditate unde quam minus doloremque aut consequuntur optio deserunt, ipsam recusandae praesentium vitae commodi molestias facilis labore quaerat officia! Rem, est nostrum.
+              </p>
+            </div>
+            <div id="Introduction To Java" class = "IntroJava">
+              <h1>Introduction To Java</h1>
+              <p>
+                What is Java?
+                Java is a very popular pro
+                JavaScript is the world's most popular programming language.
+                JavaScript is the programming language of the Web. JavaScript is
+                easy to learn. This tutorial will teach you JavaScript from
+                basic to advanced.
+              </p>
+            </div>
+            <div id="Introduction To Java" class = "IntroJava">
+              <h1>Introduction To Java</h1>
+              <p>
+                What is Java?
+                Java is a very popular pro
+                JavaScript is the world's most popular programming language.
+                JavaScript is the programming language of the Web. JavaScript is
+                easy to learn. This tutorial will teach you JavaScript from
+                basic to advanced.
+              </p>
+            </div>
+            <div id="Introduction To Java" class = "IntroJava">
+              <h1>Introduction To Java</h1>
+              <p>
+                What is Java?
+                Java is a very popular pro
+                JavaScript is the world's most popular programming language.
+                JavaScript is the programming language of the Web. JavaScript is
+                easy to learn. This tutorial will teach you JavaScript from
+                basic to advanced.
+              </p>
+            </div>
+            <div id="Types Of Methods java-method" class = "IntroJava">
+              <h1>Types Of Methods</h1>
+              <p>
+                What is Method?
+                Java is a very popular pro
                 JavaScript is the world's most popular programming language.
                 JavaScript is the programming language of the Web. JavaScript is
                 easy to learn. This tutorial will teach you JavaScript from
