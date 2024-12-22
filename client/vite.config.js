d1ac254c8c1151bd409be175e860a321f9610b86
UjjwalSingh16 <30306011+hony09@users.noreply.github.com>
2024-12-22 23:37:28 +0530

    done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: './',  // Ensure Vite points to the root of the project
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Output directory for production build
+  },
 })
