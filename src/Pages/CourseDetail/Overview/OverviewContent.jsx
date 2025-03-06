20b35f3bceb45010380b55e092f814acdb44e3c1
sudhanshu2070 <sssallen2016@gmail.com>
2025-03-06 14:34:44 +0530

    Added CourseDetail Page 0.3

--- src/Pages/CourseDetail/Overview/OverviewContent.jsx
+++ src/Pages/CourseDetail/Overview/OverviewContent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './OverviewContent.css'; // Import the corresponding CSS file
+import './OverviewContent.css';
 
 const OverviewContent = ({ details, features }) => {
   return (
@@ -20,11 +20,16 @@ const OverviewContent = ({ details, features }) => {
         </ul>
 
         <h3>Course Syllabus</h3>
-        <ul>
-          {details.syllabus.map((topic, index) => (
-            <li key={index}>{topic}</li>
-          ))}
-        </ul>
+        {details.syllabus.map((section, index) => (
+          <div key={index} className="syllabus-section">
+            <h4>{section.title}</h4>
+            <ol>
+              {section.topics.map((topic, idx) => (
+                <li key={idx}>{topic}</li>
+              ))}
+            </ol>
+          </div>
+        ))}
 
         <h3>Sample Video</h3>
         <iframe
@@ -39,39 +44,39 @@ const OverviewContent = ({ details, features }) => {
 
       {/* Right Section (30%) */}
       <div className="right-section">
-  <h3>Course Features</h3>
-  <table>
-    <tbody>
-      <tr>
-        <td>Lectures</td>
-        <td>{features.lectures}</td>
-      </tr>
-      <tr>
-        <td>Quiz</td>
-        <td>{features.quiz}</td>
-      </tr>
-      <tr>
-        <td>Duration</td>
-        <td>{features.duration}</td>
-      </tr>
-      <tr>
-        <td>Skill Level</td>
-        <td>{features.skillLevel}</td>
-      </tr>
-      <tr>
-        <td>Language</td>
-        <td>{features.language}</td>
-      </tr>
-      <tr>
-        <td>Students</td>
-        <td>{features.students}</td>
-      </tr>
-      <tr>
-        <td>Assessments</td>
-        <td>{features.assessments}</td>
-      </tr>
-    </tbody>
-  </table>
+        <h3>Course Features</h3>
+        <table>
+          <tbody>
+            <tr>
+              <td>Lectures</td>
+              <td>{features.lectures}</td>
+            </tr>
+            <tr>
+              <td>Quiz</td>
+              <td>{features.quiz}</td>
+            </tr>
+            <tr>
+              <td>Duration</td>
+              <td>{features.duration}</td>
+            </tr>
+            <tr>
+              <td>Skill Level</td>
+              <td>{features.skillLevel}</td>
+            </tr>
+            <tr>
+              <td>Language</td>
+              <td>{features.language}</td>
+            </tr>
+            <tr>
+              <td>Students</td>
+              <td>{features.students}</td>
+            </tr>
+            <tr>
+              <td>Assessments</td>
+              <td>{features.assessments}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
