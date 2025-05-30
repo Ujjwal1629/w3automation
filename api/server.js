f423f220f525a898cdcc126d12d9357b9125d117
Ujjwal1629 <ujjwalsingh1629@gmail.com>
2025-05-30 14:00:43 +0530

    BE mongo setup done

--- api/server.js
+++ api/server.js
@@ -2,14 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import authRoutes from './routes/auth.js';
 
-dotenv.config();
+dotenv.config({
+  path: process.env.NODE_ENV === 'production' ? '.env.prod' : '.env'
+});
 
 const app = express();
 
-// CORS setup to allow frontend origins for both dev and production
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? ['https://w3automation.vercel.app', 'https://journeytoautomation.org']
@@ -18,7 +20,6 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5001;
