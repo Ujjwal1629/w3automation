e42025a180c14dcb3e12e3125b5ae229be23ae0b
Hunny Chahar <hunnychahar@Hunnys-MacBook-Air.local>
2025-07-05 12:47:19 +0530

    BE cors issue fix

--- api/server.js
+++ api/server.js
@@ -14,12 +14,20 @@ dotenv.config({
 
 const app = express();
 
-// Configure CORS
+// Configure CORS with all possible domain variations
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://w3automation.vercel.app', 'https://journeytoautomation.org']
+    ? [
+        'https://w3automation.vercel.app',
+        'https://journeytoautomation.org',
+        'https://www.journeytoautomation.org',
+        'http://journeytoautomation.org',
+        'http://www.journeytoautomation.org'
+      ]
     : 'http://localhost:5173',
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
